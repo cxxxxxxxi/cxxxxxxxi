6b864329fabd836e55b160fc22eb31054c7d3da7
ShuruiFang (cccxi from NKU) <145469185+cxxxxxxxi@users.noreply.github.com>
2024-07-28 23:40:49 +0800

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,6 @@
+INSERT INTO `user` (`id`, `username`, `password`, `phone`, `header`) VALUES (1, 'zhangsn', '123', '156020256663', NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `phone`, `header`) VALUES (2, 'zhangsn', '111', '156020256663', NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `phone`, `header`) VALUES (12, 'zhangsn', '22', '156020256663', 'zhangsn.jpg');
+INSERT INTO `user` (`id`, `username`, `password`, `phone`, `header`) VALUES (14, '1', '1', '156020256663', '1.jpg');
+INSERT INTO `user` (`id`, `username`, `password`, `phone`, `header`) VALUES (15, 'admin', '666', '15602025663', '1.jpg');
+INSERT INTO `user` (`id`, `username`, `password`, `phone`, `header`) VALUES (17, '小当家', '22', '156020256663', '小当家.jpg');
